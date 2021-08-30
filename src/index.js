465275478c42b79bee879c4cb1ee8c217e25f918
ConstantineTU <8kozmo8@gmail.com>
2021-08-31 04:43:57 +0500

    init: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+// module.exports = function check(str, bracketsConfig) {
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let opBracket = bracketsConfig.map((item) => item[0]);
+  let clBracket = bracketsConfig.map((item) => item[1]);
+  let brackets = [];
+  let isTrue = true;
+
+  for (let index = 0; index <= str.length; index++) {
+    if (opBracket.includes(str[index])) {
+      if (clBracket.includes(str[index])) {
+        if (clBracket.indexOf(str[index]) === brackets[brackets.length - 1]) {
+          brackets.pop()
+        } else { brackets.push(opBracket.indexOf(str[index])) };
+      } else { brackets.push(opBracket.indexOf(str[index])) };
+      continue;
+    }
+    if (clBracket.includes(str[index])) { if (clBracket.indexOf(str[index]) != brackets.pop()) { isTrue = false; break; } }
+  }
+  return (isTrue && brackets.length == 0);
 }
